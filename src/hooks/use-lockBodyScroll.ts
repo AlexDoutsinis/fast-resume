f887e06b36f81434b9f447d1b23882907b1ed310
Alexiosdut <Alexiosdut@gmail.com>
2020-09-05 17:04:52 +0300

    useLockBodyScroll.ts: hide templates section

--- src/hooks/use-lockBodyScroll.ts
+++ src/hooks/use-lockBodyScroll.ts
@@ -4,10 +4,12 @@ export function useLockBodyScroll() {
   useLayoutEffect(() => {
     const heroSelector = document.querySelectorAll("section")[0]
     const featuresSelector = document.querySelectorAll("section")[1]
+    const templatesSelector = document.querySelectorAll("section")[2]
     const footerSelector = document.querySelector("footer")
 
     heroSelector.style.opacity = "0"
     featuresSelector.style.opacity = "0"
+    templatesSelector.style.opacity = "0"
     footerSelector.style.opacity = "0"
 
     const originalStyle = window.getComputedStyle(document.body).overflow
@@ -19,6 +21,7 @@ export function useLockBodyScroll() {
 
       heroSelector.style.opacity = "1"
       featuresSelector.style.opacity = "1"
+      templatesSelector.style.opacity = "1"
       footerSelector.style.opacity = "1"
     }
   }, [])
