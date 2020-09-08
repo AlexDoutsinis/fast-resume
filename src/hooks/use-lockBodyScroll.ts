d72844274e95fa7c2db97a954ac6a198836a5f54
Alexiosdut <Alexiosdut@gmail.com>
2020-09-08 18:58:49 +0300

    use-lockBodyScroll: hide header

--- src/hooks/use-lockBodyScroll.ts
+++ src/hooks/use-lockBodyScroll.ts
@@ -2,11 +2,13 @@ import { useLayoutEffect } from "react"
 
 export function useLockBodyScroll() {
   useLayoutEffect(() => {
+    const headerSelector = document.querySelector("header")
     const heroSelector = document.querySelectorAll("section")[0]
     const featuresSelector = document.querySelectorAll("section")[1]
     const templatesSelector = document.querySelectorAll("section")[2]
     const footerSelector = document.querySelector("footer")
 
+    headerSelector.style.opacity = "0"
     heroSelector.style.opacity = "0"
     featuresSelector.style.opacity = "0"
     templatesSelector.style.opacity = "0"
@@ -19,6 +21,7 @@ export function useLockBodyScroll() {
     return () => {
       document.body.style.overflow = originalStyle
 
+      headerSelector.style.opacity = "1"
       heroSelector.style.opacity = "1"
       featuresSelector.style.opacity = "1"
       templatesSelector.style.opacity = "1"
