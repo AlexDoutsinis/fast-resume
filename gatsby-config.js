5aa92fbb28028a77b1db71cfe4873556077d8532
Alexiosdut <Alexiosdut@gmail.com>
2020-09-21 13:50:33 +0300

    remove unused plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,6 @@ module.exports = {
         },
       },
     },
-    'gatsby-disable-404',
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
@@ -69,8 +68,5 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
   ],
 }
